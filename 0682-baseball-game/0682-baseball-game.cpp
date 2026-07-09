77e48a634aae25fe627c8296528ad55409949f7d
Prasenjit Majumder <coding.prasenjit@gmail.com>
2026-07-09 23:55:36 +0530

    Time: 0 ms (100%), Space: 11.8 MB (95.18%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int calPoints(vector<string>& nums) {
 
+        int sum = 0;
         int n = nums.size();
 
         vector<int> ans;
@@ -13,18 +14,18 @@ public:
             if (val == "+") {
 
                 ans.push_back(ans[ans.size() - 1] + ans[ans.size() - 2]);
+                sum += ans.back();
             } else if (val == "D") {
                 ans.push_back((ans[ans.size() - 1] * 2));
+                sum += ans.back();
             } else if (val == "C") {
+                sum -= ans.back();
                 ans.pop_back();
             } else {
                 ans.push_back(stoi(val));
+                sum += ans.back();
             }
         }
-
-        int sum = 0;
-
-        for(int i: ans) sum += i;
 
         return sum;
     }
