0819af20f37b90c3f929a2b45ccf3a104cfd8e20
Prasenjit Majumder <coding.prasenjit@gmail.com>
2026-07-30 10:06:07 +0530

    Time: 0 ms (100%), Space: 10 MB (90.03%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1 = 0, prev2 = 0;
+
+        for (int money : nums) {
+            int take = prev2 + money;
+            int skip = prev1;
+            int curr = max(take, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
