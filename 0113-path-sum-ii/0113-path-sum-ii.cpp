35fd5c869825677c176468af1339a659a2428786
Prasenjit Majumder <coding.prasenjit@gmail.com>
2026-09-14 23:04:19 +0530

    Time: 3 ms (39.95%), Space: 21.1 MB (39.63%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -6,45 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-
     vector<vector<int>> ans;
     vector<int> path;
 
-    void solve(TreeNode* root, int targetSum){
+    void solve(TreeNode* root, int targetSum) {
 
-        if(root == nullptr) return;
+        if (root == nullptr)
+            return;
 
-        path.push_back(root -> val);
+        path.push_back(root->val);
 
-        if(root -> left == nullptr && root -> right == nullptr){
+        if (root->left == nullptr && root->right == nullptr) {
 
-            if(targetSum == root -> val){
+            if (targetSum == root->val) {
 
                 ans.push_back(path);
             }
         }
 
-        solve(root -> left, targetSum - root -> val);
-        solve(root -> right, targetSum - root -> val);
+        solve(root->left, targetSum - root->val);
+        solve(root->right, targetSum - root->val);
 
         path.pop_back();
-
-
-
-
     }
-
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
 
         solve(root, targetSum);
 
         return ans;
-        
     }
 };
